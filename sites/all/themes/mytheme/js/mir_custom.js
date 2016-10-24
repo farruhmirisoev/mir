21a9e23a06c5c6bfd19ff8ec1987fc96dbe9dc10
farruh Mirisoev <farruh.mirisoev@gmail.com>
2016-10-24 16:30:48 +0500

    stili titles posel multiyazika

--- sites/all/themes/mytheme/js/mir_custom.js
+++ sites/all/themes/mytheme/js/mir_custom.js
@@ -1,21 +1,5 @@
 (function($) {
     $(document).ready(function() {
-        // if ($('.our-works-slider').length > 0) {
-        //     var our_work_slider = $('.our-works-slider').bxSlider({
-        //         minSlides: 5,
-        //         maxSlides: 5,
-        //         slideWidth: 178,
-        //         auto: true,
-        //         pause: 7000,
-        //         nextText: '&#xf061;',
-        //         prevText: '&#xf060;',
-        //         autoHover: true,
-        //         pager: false,
-        //         controls: true,
-        //         adaptiveHeight: true,
-        //     });
-        // }
-
         $('.view-id-main_slider.view-display-id-block_1 .view-content').slick({
             arrows: false,
             infinite: false,
@@ -59,6 +43,7 @@
             slidesToScroll: 1,
         });
 
+        // porfolio---------------------
         $('.mir-portfolio-slider__main .field-items').slick({
             dots: false,
             infinite: true,
@@ -85,5 +70,54 @@
         });
 
 
-    });
+        //  certificate slider-----------------
+        $('.view-view-cert.view-display-id-block')
+        .prepend('<div class="certificate-slider-nav certificate-slider-nav--prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>')
+        .prepend('<div class="certificate-slider-nav certificate-slider-nav--next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>');
+
+        var certificate_slider = $('.view-view-cert.view-display-id-block .view-content').slick({
+            dots: false,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        });
+        $('.certificate-slider-nav--prev').click(function(){
+            certificate_slider.slick('slickPrev');
+        });
+        $('.certificate-slider-nav--next').click(function(){
+            certificate_slider.slick('slickNext');
+        });
+
+
+
+        // stats-object --------------------------------------
+
+        $('.stats-object__number').each(function(index, elem) {
+            var $elem = $(elem);
+            $elem.data('count', $elem.html());
+            $elem.html('0');
+        });
+
+        $(window).scroll(function() {
+           if ($('.stats-object').length && !$('.stats-object').data('processed') && $(this).scrollTop() >= $('.stats-object').offset().top - $(window).height()) {
+                $('.stats-object__number').each(function(index, elem) {
+                    var $elem = $(elem);
+                    $elem.animateNumber({number: $elem.data('count')}, 1000);
+                });
+                $('.stats-object').data('processed', true);
+           }
+        });
+
+
+        //set all img titles as alts
+        $('img').each(function(){
+          var ImgAlt = $(this).attr('alt');
+          $(this).attr('title',ImgAlt);
+        })
+
+
+    }); // doc ready
 })(jQuery);
